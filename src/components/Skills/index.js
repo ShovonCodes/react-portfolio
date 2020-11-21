927fc40aca8633b2b7675622a93f41a3c3dd3943
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-11-21 12:16:30 +0600

    Skills component

---
+++ src/components/Skills/index.js
@@ -0,0 +1,41 @@
+import React, { useContext } from "react";
+import "./index.scss";
+import { ThemeContext } from "../../context/ThemeContext";
+
+const Skills = () => {
+  const {
+    changeTheme,
+    startThemeChangeTimer,
+    stopThemeChangeTimer,
+  } = useContext(ThemeContext);
+
+  function handleMouseEnter(e) {
+    const type = e.nativeEvent.target.id;
+    if (type) {
+      changeTheme(type);
+    }
+  }
+
+  return (
+    <div className="skills">
+      <h5>Skills</h5>
+      <ul
+        onMouseEnter={stopThemeChangeTimer}
+        // onMouseLeave={startThemeChangeTimer}
+        onMouseOver={handleMouseEnter}
+      >
+        <li id="python">Python</li>
+        <li id="django">Django</li>
+        <li id="drf">Django Rest Framework</li>
+        <li id="postman">Postman</li>
+        <li id="javascript">JavaScript</li>
+        <li id="react">React</li>
+        <li id="git">Git</li>
+        <li id="aws">AWS</li>
+        <li id="bs">Beautiful Soup</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Skills;
