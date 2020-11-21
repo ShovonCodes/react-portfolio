d09a3afebdbc04c7862fb61b81e49323571544c1
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-11-21 12:27:46 +0600

    Works tab components

---
+++ src/pages/Works/data.js
@@ -0,0 +1,80 @@
+export const projects = [
+  {
+    title: "Personal Blog",
+    images: [
+      "https://i.ibb.co/wwg9HNG/meraki1.png",
+      "https://i.ibb.co/rsr0bCT/meraki2.png",
+      "https://i.ibb.co/7bZxwNW/meraki3.png",
+      "https://i.ibb.co/K9vG2fV/meraki4.png",
+    ],
+    sourceCodeLink: "https://github.com/Shovon588/words-with-meraki",
+    demoLink: "http://wordswithmeraki.pythonanywhere.com/",
+    type: "django",
+  },
+  {
+    title: "Twing",
+    images: [
+      "https://i.ibb.co/Qkngn0r/twing1.jpg",
+      "https://i.ibb.co/Yt02X3s/twing2.jpg",
+      "https://i.ibb.co/xzVLNM7/twing3.jpg",
+    ],
+    sourceCodeLink: "https://github.com/Shovon588/project-anonymsg",
+    demoLink: "https://twing.herokuapp.com/",
+    type: "django",
+  },
+  {
+    title: "Codeforces Profile Analyzer",
+    images: [
+      "https://i.ibb.co/XZ5Hkvt/cfpa1.png",
+      "https://i.ibb.co/L0TMqpk/cfpa2.png",
+    ],
+    sourceCodeLink: "https://github.com/Shovon588/codeforces_profile_analyzer",
+    demoLink: "http://cfpa.pythonanywhere.com/",
+    type: "django",
+  },
+  {
+    title: "Corona Info",
+    images: [
+      "https://i.ibb.co/Yp1QPYH/corona1.png",
+      "https://i.ibb.co/Twsh6jT/corona2.png",
+      "https://i.ibb.co/84PPGbc/corona3.png",
+    ],
+    sourceCodeLink: "https://github.com/Shovon588/corona-info",
+    demoLink: "http://coronainfobd.pythonanywhere.com/",
+    type: "django",
+  },
+  {
+    title: "React Portfolio",
+    images: [
+      "https://i.ibb.co/wdNfGq3/portfolio1.png",
+      "https://i.ibb.co/qpRkftf/portfolio2.png",
+      "https://i.ibb.co/Vq5FfFd/portfolio3.png",
+      "https://i.ibb.co/y5v2xM9/portfolio4.png",
+    ],
+    sourceCodeLink: "https://github.com/TheCoderDream/react-portfolio",
+    demoLink: "https://emrebaskan.netlify.app/",
+    type: "react",
+  },
+  {
+    title: "Weather Info",
+    images: [
+      "https://i.ibb.co/Lp41vK6/weather1.jpg",
+      "https://i.ibb.co/r6JHdRY/weather2.jpg",
+      "https://i.ibb.co/FqbP9Zp/weather3.jpg",
+    ],
+    sourceCodeLink: "https://github.com/Shovon588/react-weather-app",
+    demoLink: "https://weather588.herokuapp.com",
+    type: "react",
+  },
+  {
+    title: "To Do List",
+    images: [
+      "https://i.ibb.co/ySs4fm2/todo1.png",
+      "https://i.ibb.co/g99f6tC/todo2.png",
+      "https://i.ibb.co/mBQSpdB/todo3.png",
+    ],
+    sourceCodeLink: "https://github.com/Shovon588/react-to-do-list",
+    demoLink: "#",
+    type: "DRF",
+  },
+];
