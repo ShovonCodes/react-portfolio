d9c4f38a9b5c5f29f92b2ce684200c2a612de112
Shovon <minul@vimond.com>
2022-02-13 21:41:27 +0600

    Updated resume pages

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -11,7 +11,7 @@ const Resume = () => {
 			</Title>
 
 			<ResumeItem
-				date={'2016 – 2020'}
+				date={'2016 – 2021'}
 				title={'Rajshahi University of Engineering & Technlogy'}
 				company={'Rajshahi, Bangladesh'}>
 				<p
@@ -42,23 +42,57 @@ const Resume = () => {
 				{/*Resume - Total Commercial Experience {moment().diff('09/01/2016', 'years', true).toFixed(2)} years*/}
 				<span style={{ paddingLeft: '10%' }}>EXPERIENCE</span>
 			</Title>
+
 			<ResumeItem
-				date={'Jun 2020 - Present'}
-				title={'ENALO, HYDERABAD, INDIA'}
-				company={'Django Backend Developer Intern (Remote)'}>
-				• Created scalable restful APIs that powers the smooth workflow
-				in the frontend.
-				<br />• Designed and built the payment page frontend, a payment
-				button widget, integrated Cashfree payment gateway.
+				date={'Apr 2021 - Current'}
+				title={'Software Development Engineer (Full-time)'}
+				company={'Craftsmen Ltd., Dhaka, Bangladesh'}>
+				- Working as a Full Stack Developer <br />
+				- Responsible for developing Frontend and Backend <br />
+				- Working with teams in major European company <br />
+				- Working with latest tech stack like ReactJs, AWS (serverless) etc <br />
+				- Maintaining and improving legacy code <br />
+				- Developing new features to the project as per assignment <br />
+				- Making life easier for major broadcasting industries through developing OTT platform <br />
+				- Leverage knowledge on JavaScript, TypeScript, ReactJs, AWS, Serverless, Terraform etc. 
 			</ResumeItem>
+
+			<ResumeItem
+				date={'Mar 2021 - Mar 2021'}
+				title={'Junior Software Engineer (Full-time)'}
+				company={'Field Buzz (Field Information Solutions), Dhaka, Bangladesh'}>
+				- Worked as a Django backend developer <br />
+				- Maintained existing code and reported to senior engineers
+			</ResumeItem>
+
+
+			<ResumeItem
+				date={'Jun 2020 - Feb 2021'}
+				title={'Django Backend Developer (Intern)'}
+				company={'Enalo, Hyderabad, India'}>
+				- Worked as a remote Full-Stack developer <br />
+				- Have had hands-on experience working on a real-life project <br />
+				- Worked in Django, Django Rest Framework, VanillaJs and ReactJs <br />
+				- Had hands on experience on AWS technologies <br />
+				- Built and deployed 30+ backend API's that is powering web and mobile application <br />
+				- Designed and built responsive and user-friendly web application <br />
+				- Had continuous collaboration and discussion with other teammates <br />
+				- Designed and built the landing page, widget and integrated Cashfree payment gateway. <br />
+				- Leverage knowledge on Django, REST API, AWS, ReactJs, Payment Gateway etc. <br />
+			</ResumeItem>
+
 			<ResumeItem
 				date={'May 2019 - Jun 2019'}
-				title={"SELISE ROCKIN' SOFTWARE, DHANMONDI, DHAKA"}
-				company={'Software Engineer Intern'}>
-				• Played a prime role in building the frontend and backend for
-				an MCQ based examination system.
-				<br />• Collaborated with team and maintained the workflow and
-				deadline using Scrum.
+				title={'Software Engineer (Intern)'}
+				company={`SELISE ROCKIN' SOFTWARE, Dhaka, Bangladesh`}>
+				- Learnt a lot of industry best practices and software development life cycle <br />
+				- Learnt different tools and frameworks required for the development <br />
+				- Designed, proposed, and built an online MCQ test application <br />
+				- Reported, updated, and work according to feedback <br />
+				- Collaborated and had continuous discussions with senior developers for different tasks <br />
+				- Was mentored by the CTO and other senior Engineers with decades of experience <br />
+				- Experienced a great company environment with excellent culture <br />
+				- Leverage knowledge on Jira, Balsamiq, BitBucket, PHP, etc. <br />
 			</ResumeItem>
 
 			<Title icon={'fa fa-newspaper'}>
