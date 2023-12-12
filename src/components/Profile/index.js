26381fde1668d225a0b431f97e089e6df4d5f61d
Shovon <shovon@cuttingroom.com>
2023-12-12 13:54:01 +0600

    Changed profile image, updated website title, added image magnification

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { Lightbox } from "react-modal-image"
 import './index.scss';
 import TypedText from '../TypedText';
 import SocialLinks from '../SocialLinks';
@@ -6,7 +7,11 @@ import Info from '../Info';
 import TopSkills from '../Skills/TopSkills';
 import { ThemeContext } from '../../context/ThemeContext';
 
+const IMAGE_URL = 'https://i.ibb.co/19mQnPf/headshot.jpg';
+
 const Profile = () => {
+	const [openImage, setOpenImage] = useState(false)
+
 	const { backgroundImage, type } = useContext(ThemeContext);
 	return (
 		<div className='profile'>
@@ -15,7 +20,7 @@ const Profile = () => {
 					className={`profile__photo`}
 					style={{ backgroundImage: `url(${backgroundImage})` }}
 				/>
-				<img src='https://i.ibb.co/SVC1Hr3/dp-small.jpg' alt={type} />
+				<img src={IMAGE_URL} alt={type} onClick={() => setOpenImage(true)} />
 			</div>
 			<div className='profile__content'>
 				<div className='profile__title'>Md. Minul Islam Shovon</div>
@@ -46,6 +51,14 @@ const Profile = () => {
 					<span>Contact Me</span>
 				</a>
 			</div>
+
+			{openImage &&
+				<Lightbox
+					large={IMAGE_URL}
+					hideZoom
+					onClose={() => setOpenImage(false)}
+			/>}
+
 		</div>
 	);
 };
